d4f8b1f73f014ca585fd036513115b75f576573b
Jerry Dai <jdai315@gmail.com>
2017-09-18 19:20:08 -0400

    first commit: IA files

---
+++ estellakim.php
@@ -0,0 +1,46 @@
+<!doctype html>
+
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Estella Kim</title>
+
+        <link href="css/styles.css" rel="stylesheet">
+        <link href="css/subpage.css" rel="stylesheet">
+        <link href="css/navigation.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <h1>Estella Kim</h1>
+
+        <h2>Coursework</h2>
+        <p>
+            Marketing Research & Analytics, Principles of Marketing, Financial Accounting, Managerial Accounting, Principles of Economics, Microeconomics
+        </p>
+
+        <h2>Skills</h2>
+        <p> 
+            I know HTML, CSS, JavaScript, Java, and Python. I speak English, Korean, and Japanese. I know these Adobe programs: InDesign, Dreamweaver, and Photoshop.
+        </p>
+
+        <h2>University of Rochester Wilson Commons Student Activities Center - Marketing Intern</h2>
+        <p>
+            I develop and implement marketing strategies and assist in the management of content on the Student Activities Website. I also design and distribute marketing materials throughout campus and maintain social media outlets for campus services and programs.
+        </p>
+
+        <h2>University of Rochester Chemistry Department - Office Assistant & Junior Web Developer</h2>
+        <p>
+            I created departmental publications, flyers & posters. I was also responsible for revising the Chemistry department website with weekly updates/news/publications
+        </p>
+
+        <h2>University of Rochester- Student Fundraiser</h2>
+        <p>
+            I utilized a donor centric approach to efficiently and effectively communicate the goals and objectives of the University Alumni and Donors, in order to meet the philanthropic goals in terms of annual contacts, pledges, and financial support.
+        </p>
+
+    </body>
+    
+</html>
---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Group #2 Assignment #4</title>
+
+        <link href="css/styles.css" rel="stylesheet">
+        <link href="css/navigation.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <div class="container">
+
+            <header>
+                <div class="heading">
+                    <h1>Group #2 Assignment #4</h1>
+                </div><!--closes heading class-->
+            </header>
+
+            <nav class="menu">
+                <?php include 'inc/navigation.php'; ?>
+            </nav>
+
+            <div class="content">
+
+                <div class="well">
+                    <h2>Jerry Dai</h2>
+                    <p>
+                        Hi my name is Jerry and I am a junior at the U of R. I'm from Queens, NY but my parents moved to Brooklyn from China when they were in their early 20s.
+                    </p>
+                </div><!--closes well class-->
+
+                <div class="well">
+                    <h2>Philip Kallinos</h2>
+                    <p>
+                        I am from Lindenhurst, New York. My parents are both immigrants. My father is from Cyprus and my mother is from Thailand. I also have one older sister.
+                    </p>
+                </div><!--closes well class-->
+
+                <div class="well">
+                    <h2>Estella Kim</h2>
+                    <p> 
+                        I know HTML, CSS, JavaScript, Java, and Python. I speak English, Korean, and Japanese. I know these Adobe programs: InDesign, Dreamweaver, and Photoshop.
+                    </p>
+                </div><!--closes well class-->
+
+            </div><!--closes content class-->
+
+            <footer>
+                <?php include 'inc/footer.php'; ?>
+            </footer>
+
+        </div><!--closes the container class-->
+
+    </body>
+
+</html>
