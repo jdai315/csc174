f6a852ca999a79c310f492fcc8b64a8bc440efc9
ekim62 <32079727+ekim62@users.noreply.github.com>
2017-09-18 19:41:37 -0400

    Update navigation.php

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,3 +1,3 @@
 <?php
-echo <a href="jerrydai.php"Jerry Dai</a> <a href="philipkallinos.php" Phillip Kallinos</a> <a href="estellakim.php" Estella Kim </a>'
+echo '<a href="jerrydai.php"Jerry Dai</a> <a href="philipkallinos.php" Phillip Kallinos</a> <a href="estellakim.php" Estella Kim </a>'
 ?>
