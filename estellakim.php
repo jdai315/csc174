6c923c73c8fbe2f884b774c0f92c57f50f984039
Jerry Dai <jdai315@gmail.com>
2017-09-18 19:25:39 -0400

    IA: added PHP include lines to HTML

--- estellakim.php
+++ estellakim.php
@@ -14,33 +14,49 @@
 
     <body>
 
-        <h1>Estella Kim</h1>
+        <div class="container">
 
-        <h2>Coursework</h2>
-        <p>
-            Marketing Research & Analytics, Principles of Marketing, Financial Accounting, Managerial Accounting, Principles of Economics, Microeconomics
-        </p>
+            <header>
+                <div class="heading">
+                    <h1>Estella Kim</h1>
+                </div><!--closes heading class-->
+            </header>
 
-        <h2>Skills</h2>
-        <p> 
-            I know HTML, CSS, JavaScript, Java, and Python. I speak English, Korean, and Japanese. I know these Adobe programs: InDesign, Dreamweaver, and Photoshop.
-        </p>
+            <nav class="menu">
+                <?php include 'inc/navigation.php'; ?>
+            </nav>
 
-        <h2>University of Rochester Wilson Commons Student Activities Center - Marketing Intern</h2>
-        <p>
-            I develop and implement marketing strategies and assist in the management of content on the Student Activities Website. I also design and distribute marketing materials throughout campus and maintain social media outlets for campus services and programs.
-        </p>
+            <h2>Coursework</h2>
+            <p>
+                Marketing Research & Analytics, Principles of Marketing, Financial Accounting, Managerial Accounting, Principles of Economics, Microeconomics
+            </p>
 
-        <h2>University of Rochester Chemistry Department - Office Assistant & Junior Web Developer</h2>
-        <p>
-            I created departmental publications, flyers & posters. I was also responsible for revising the Chemistry department website with weekly updates/news/publications
-        </p>
+            <h2>Skills</h2>
+            <p> 
+                I know HTML, CSS, JavaScript, Java, and Python. I speak English, Korean, and Japanese. I know these Adobe programs: InDesign, Dreamweaver, and Photoshop.
+            </p>
 
-        <h2>University of Rochester- Student Fundraiser</h2>
-        <p>
-            I utilized a donor centric approach to efficiently and effectively communicate the goals and objectives of the University Alumni and Donors, in order to meet the philanthropic goals in terms of annual contacts, pledges, and financial support.
-        </p>
+            <h2>University of Rochester Wilson Commons Student Activities Center - Marketing Intern</h2>
+            <p>
+                I develop and implement marketing strategies and assist in the management of content on the Student Activities Website. I also design and distribute marketing materials throughout campus and maintain social media outlets for campus services and programs.
+            </p>
 
+            <h2>University of Rochester Chemistry Department - Office Assistant & Junior Web Developer</h2>
+            <p>
+                I created departmental publications, flyers & posters. I was also responsible for revising the Chemistry department website with weekly updates/news/publications
+            </p>
+
+            <h2>University of Rochester- Student Fundraiser</h2>
+            <p>
+                I utilized a donor centric approach to efficiently and effectively communicate the goals and objectives of the University Alumni and Donors, in order to meet the philanthropic goals in terms of annual contacts, pledges, and financial support.
+            </p>
+
+            <footer>
+                <?php include 'inc/footer.php'; ?>
+            </footer>
+
+        </div><!--closes the container class-->
+        
     </body>
-    
+
 </html>
--- jerrydai.php
+++ jerrydai.php
@@ -6,31 +6,47 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Jerry Dai</title>
-        
+
         <link href="css/styles.css" rel="stylesheet">
         <link href="css/subpage.css" rel="stylesheet">
         <link href="css/navigation.css" rel="stylesheet">
     </head>
 
     <body>
-        
-        <h1>Jerry Dai</h1>
-        
-        <h2>Personal Bio</h2>
-        <p>
-            Hi my name is Jerry and I am a junior at the U of R. I'm from Queens, NY but my parents moved to Brooklyn from China when they were in their early 20s. My hobbies are music, lifting, and cooking. My favorite foods include halal, vodka slices, and sushi. This past summer I worked at the Greater Flushing Chamber of Commerce, where I developed a neighborhood web portal to promote Flushing, NY as part of a district marketing campaign.
-        </p>
-        
-        <h2>LinkedIn Bio</h2>
-        <p>
-            Junior at the University of Rochester interested in the importance of technology in businesses. On the fun side of things, I'm a drummer for the Rock Repertory Ensemble at the University of Rochester, I participated in the "Code Suisse" coding challenge at Credit Suisse, I've been on live TV playing piano at a talent show, and I've spent one summer as a paddler on the NY Metro Athletic Dragons dragon-boating team.
-        </p>
-        
-        <h2>My Pet Fish Bolt</h2>
-        <p>
-            I have a pet betta fish named Bolt. I named him Bolt because he is blue, and at the store, I saw him only swimming vertically.
-        </p>
-        
+
+        <div class="container">
+
+            <header>
+                <div class="heading">
+                    <h1>Jerry Dai</h1>
+                </div><!--closes heading class-->
+            </header>
+
+            <nav class="menu">
+                <?php include 'inc/navigation.php'; ?>
+            </nav>
+
+            <h2>Personal Bio</h2>
+            <p>
+                Hi my name is Jerry and I am a junior at the U of R. I'm from Queens, NY but my parents moved to Brooklyn from China when they were in their early 20s. My hobbies are music, lifting, and cooking. My favorite foods include halal, vodka slices, and sushi. This past summer I worked at the Greater Flushing Chamber of Commerce, where I developed a neighborhood web portal to promote Flushing, NY as part of a district marketing campaign.
+            </p>
+
+            <h2>LinkedIn Bio</h2>
+            <p>
+                Junior at the University of Rochester interested in the importance of technology in businesses. On the fun side of things, I'm a drummer for the Rock Repertory Ensemble at the University of Rochester, I participated in the "Code Suisse" coding challenge at Credit Suisse, I've been on live TV playing piano at a talent show, and I've spent one summer as a paddler on the NY Metro Athletic Dragons dragon-boating team.
+            </p>
+
+            <h2>My Pet Fish Bolt</h2>
+            <p>
+                I have a pet betta fish named Bolt. I named him Bolt because he is blue, and at the store, I saw him only swimming vertically.
+            </p>
+
+            <footer>
+                <?php include 'inc/footer.php'; ?>
+            </footer>
+
+        </div><!--closes the container class-->
+
     </body>
 
 </html>
