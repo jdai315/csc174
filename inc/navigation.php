383baa2971138b74eb98db61aecaf7d19d1ad796
ekim62 <32079727+ekim62@users.noreply.github.com>
2017-09-18 20:36:55 -0400

    Update navigation.php

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,7 +1,7 @@
 <?php
 echo '
 <ul>
-<li> <a href="index.php">Home</a> </li>
+<li> <a href="index.php">Index</a> </li>
 <li> <a href="jerrydai.php">Jerry Dai</a> </li>
 <li> <a href="philipkallinos.php">Philip Kallinos</a> </li>
 <li> <a href="estellakim.php">Estella Kim </a> </li> </ul>'
